3b9ccbcf8c10da38146add20438590b487a7f266
AnkoBG <33812357+AnkoBG@users.noreply.github.com>
2017-11-20 20:35:35 +0200

    Rational Numbers

---
+++ Homework_2/Homework_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumber rNum1 = new RationalNumber(66, 4);
+            Console.WriteLine(rNum1.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework_2/Homework_2/RationalNumber.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_2
+{
+    class RationalNumber
+    {
+        private int numerator, denominator;
+        public RationalNumber()
+        {
+            numerator = 0;
+            denominator = 0;
+        }
+        public RationalNumber(int num, int denom)
+        {
+            numerator = num;
+            denominator = denom;
+            this.Rationalize();
+        }
+        public decimal ToDecimal()
+        {
+            return (decimal)numerator / denominator;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0}/{1}", numerator, denominator);
+        }
+        public void Rationalize()
+        {
+            int minNum = 1;
+            int minDenom = 1;
+            for (int i = 1; i <= denominator; i++)
+            {
+                if (numerator % i == 0 && denominator % i == 0)
+                {
+                    minNum = numerator / i;
+                    minDenom = denominator / i;
+                }
+            }
+            numerator = minNum;
+            denominator = minDenom;
+        }
+        public RationalNumber Add(RationalNumber rn)
+        {
+            int newNumerator;
+            int newDenominator = 0;
+            newNumerator = numerator * rn.denominator + rn.numerator * denominator;
+            newDenominator = denominator * rn.denominator;
+            return new RationalNumber(newNumerator, newDenominator);
+        }
+    }
+}
